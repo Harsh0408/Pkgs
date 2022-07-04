877774bc6ae21d4275168abb5fbe14ab4aaeba44
Lucifer409 <82993532+Harsh0408@users.noreply.github.com>
2022-07-04 21:44:50 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,4 +1,5 @@
-figlet Start download
+figlet Start
+
 pkg install git  python python2 python3 nano curl tar zip unzip tor wget wcalc bmon golang openssl cmatrix openssh toilet sl vim zsh fortune zsh proot -y
 
 figlet Thankyou 
