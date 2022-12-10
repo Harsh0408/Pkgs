1d3c972a69041fc06ac7fae89f5f3d3be0f07ce0
Lucifer409 <82993532+Lu-cif-er@users.noreply.github.com>
2022-12-10 18:00:59 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,3 +1,8 @@
-pkg install git python python2 python3 nano curl tar zip unzip tor wget wcalc bmon golang openssl cmatrix openssh toilet sl vim zsh fortune zsh proot figlet -y
+figlet is all basic packeges
 
-figlet 👍👍
+
+pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y && pkg install python3 && pkg install fish -y && pkg install vim -y && pkg install cmatrix -y && pkg install bmon -y &&pkg install proot -y && pkg install sl && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y  && pkg install nano -y && pkg install w3m -y && pkg install hydra -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet && pkg install sl && pkg install vim && pkg install tch && pkg install zsh && pkg install fortune && pkg install zsh && apt update && apt upgrade
+  
+
+
+figlet thanks you 
