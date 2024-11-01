38b183b1b8ee14e09b7a069130a236ae80443516
Lucifer409 <82993532+darkiller409@users.noreply.github.com>
2024-11-01 21:17:16 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -4,7 +4,9 @@ figlet -f standard is all basic packeges
 
 
 pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y && pkg install python3 && pkg install fish -y && pkg install vim -y && pkg install cmatrix -y && pkg install bmon -y &&pkg install proot -y && pkg install sl && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y  && pkg install nano -y && pkg install w3m -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet -y  && pkg install sl -y && pkg install vim -y && pkg install zsh -y && pkg install fortune && pkg install zsh -y && apt update  && apt upgrade
-  
+
+pkg install python && pip install colorama && pip install requests && pip install telethon && pip install licensing && pip install rich && pip install pyrogram && pip install tgcrypto && pip install setuptools==66.0.0 && pip install termcolor && pip install pyfiglet && pip install IP2Location && pip install pytz && pip install html5lib && pip install bs4
+
 pip install lolcat
 pkg install game-repo
 pkg install science-repo
