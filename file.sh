91f61d45b13f7d54e639d2bf7d8755c91d020a25
Lucifer409 <82993532+darkiller409@users.noreply.github.com>
2024-11-01 21:07:36 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -3,16 +3,15 @@ pkg install figlet
 figlet -f standard is all basic packeges
 
 
-pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y && pkg install python3 && pkg install fish -y && pkg install vim -y && pkg install cmatrix -y && pkg install bmon -y &&pkg install proot -y && pkg install sl && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y  && pkg install nano -y && pkg install w3m -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet -y  && pkg install sl -y && pkg install vim -y && pkg install tch -y && pkg install zsh -y && pkg install fortune && pkg install zsh -y && apt update  && apt upgrade
+pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y && pkg install python3 && pkg install fish -y && pkg install vim -y && pkg install cmatrix -y && pkg install bmon -y &&pkg install proot -y && pkg install sl && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y  && pkg install nano -y && pkg install w3m -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet -y  && pkg install sl -y && pkg install vim -y && pkg install zsh -y && pkg install fortune && pkg install zsh -y && apt update  && apt upgrade
   
 pip install lolcat
 pkg install game-repo
 pkg install science-repo
-pkg install root- repo
+pkg install root-repo
 pkg install x11-repo
 pkg install tur-repo
 pkg install zphisher
-pkg install nodejs-ltsnpm install-g truecallerjs
 apt install php
 
 cd 
